ea41735231c1b667c64040c32b8bd21025179f45
kshitij <63649035+kshitij1127@users.noreply.github.com>
2021-11-30 15:24:46 +0530

    Add files via upload

---
+++ guessing.py
@@ -0,0 +1,23 @@
+import random
+number = random.randrange(1, 10)
+guess = int(input("guess a number between 1 and 10: "))
+chances = 5
+
+# guess the number from 1 to 10 5 times until correct
+
+while guess != number and chances > 0:
+    if guess > number:
+        print("too high")
+        guess = int(input("guess again: "))
+        chances -= 1
+    else: 
+        print("too low")
+        guess = int(input("guess again: "))
+        chances -= 1
+
+print("you guessed it!")
+
+if chances == 0:
+    print("game over")
+    print("the number was", number)
+
